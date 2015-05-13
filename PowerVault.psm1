6f70463e39d5f524496c264fe570d5572cf17d47
Chris Hunt <cdhunt@gmail.com>
2015-05-13 15:56:27 -0400

    Cmdlet help

--- PowerVault.psm1
+++ PowerVault.psm1
@@ -2,13 +2,11 @@
 
 <#
 .Synopsis
-   Short description
+   Return an Object containing Vault connection details.
 .DESCRIPTION
-   Long description
+   This is session variable required by all other Cmdlets.
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
+   $vault = Get-Vault -Address 127.0.0.1 -Token 46e231ee-49bb-189d-c58d-f276743ececa
 #>
 function Get-Vault
 {
@@ -17,15 +15,15 @@ function Get-Vault
     [OutputType([PSCustomObject])]
     Param
     (
-        # Param1 help description
+        # Server Address
         [Parameter(Mandatory, Position=0)]
         $Address,
 
-        # client token
+        # Client token
         [Parameter(Mandatory, Position=1)]
         $Token,
 
-        # Param2 help description
+        # Server Port
         [Parameter(Position=2)]
         [int]
         $Port = 8200
@@ -41,13 +39,26 @@ function Get-Vault
 
 <#
 .Synopsis
-   Short description
+   Access a Secret in the Vault
 .DESCRIPTION
-   Long description
+   This can return a [PSCustomObject] base don the raw Secret or attempt to return a [PSCredential] object.
 .EXAMPLE
-   Example of how to use this cmdlet
+   Get-Secret -VaultObject $vault -Path secret/hello
+
+   value
+   -----
+   world
 .EXAMPLE
-   Another example of how to use this cmdlet
+   Get-Secret -VaultObject $vault -Path secret/username -AsCredential 
+
+   UserName                       Password
+   --------                       --------
+   username   System.Security.SecureString
+.LINK
+   Get-Vault
+   https://www.vaultproject.io/docs/http/index.html
+.NOTES
+   At the current version, Vault does not yet promise backwards compatibility even with the v1 prefix. We'll remove this warning when this policy changes. We expect we'll reach API stability by Vault 0.3.
 #>
 function Get-Secret
 {
@@ -56,16 +67,17 @@ function Get-Secret
     [OutputType([Object])]
     Param
     (
-        # Param1 help description
+        # The Object containing Vault access details.
         [Parameter(Mandatory, Position=0)]
         [PSCustomObject]
         $VaultObject,
 
-        # Param1 help description
+        # The Path to the secret as you would pass to Vault Read.
         [Parameter(Mandatory, Position=1)]
         [String]
         $Path,
 
+        # Attempt to convert the Secret to a [PSCredential]. If the Secret contains a username property that will be used else the function will fall back to using the Secret name.
         [Parameter()]
         [switch]
         $AsCredential
@@ -88,16 +100,26 @@ function Get-Secret
                 $username = [string]::Empty
 
                 if ($data | Get-Member -Name username)
-                {
+                {                    
                     $username = $data.username
+                    Write-Debug "Found a username property in the results. [$username]"
                 }
                 else
                 {
                     $username = $Path.Split('/')[-1]
+                    Write-Debug "Did not find a username property, parsing path. [$username]"
                 }
 
-                New-Object -TypeName System.Management.Automation.PSCredential `
-                -ArgumentList $username, ($data.password | ConvertTo-SecureString -AsPlainText -Force)
+                if ($data | Get-Member -Name password)
+                {
+                    New-Object -TypeName System.Management.Automation.PSCredential `
+                    -ArgumentList $username, ($data.password | ConvertTo-SecureString -AsPlainText -Force)
+                }
+                else
+                {
+                    Write-Debug $result
+                    throw "The data did not contain a password property."
+                }
             }
             else
             {
